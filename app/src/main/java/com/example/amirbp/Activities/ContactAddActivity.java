a70c2b728a56a57c419a8d090375516828056101
TanvirulAlamTusher <tanvirulalam15@gmail.com>
2024-05-08 17:15:51 +0600

    add entity

--- app/src/main/java/com/example/amirbp/Activities/ContactAddActivity.java
+++ app/src/main/java/com/example/amirbp/Activities/ContactAddActivity.java
@@ -3,32 +3,69 @@ package com.example.amirbp.Activities;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-
-import com.example.amirbp.R;
 import com.example.amirbp.Utils.AppUtils;
 import com.example.amirbp.databinding.ActivityContactAddBinding;
 
 public class ContactAddActivity extends AppCompatActivity {
     ActivityContactAddBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       binding = ActivityContactAddBinding.inflate(getLayoutInflater());
-       setContentView(binding.getRoot());
+        binding = ActivityContactAddBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-       topbar();
-       allButtonTask();
+        topbar();
+        allButtonTask();
+
     }
 
     private void allButtonTask() {
-        binding.saveButtonId.setOnClickListener(view ->{
-            Toast.makeText(this, "save Click", Toast.LENGTH_SHORT).show();
+        binding.saveButtonId.setOnClickListener(view -> {
+            String caseNumber = binding.caseNumberEditTextId.getText().toString();
+            String name = binding.nameEditTextId.getText().toString();
+            String indentityType = binding.identityEditTextId.getText().toString();
+            String rank = binding.rankEditTextId.getText().toString();
+            String bpNumber = binding.bpNumberEditTextId.getText().toString();
+            String nid = binding.nidEditTextId.getText().toString();
+            String birthDay = binding.birthDayTvId.getText().toString();
+            String bankAccountNumber = binding.bankAccountNumberEditTextId.getText().toString();
+            String mobileNumber = binding.mobileNumberEditTextId.getText().toString();
+            String mobileNumber2 = binding.mobileNumber2EditTextId.getText().toString();
+            String mobileNumber3 = binding.mobileNumber3EditTextId.getText().toString();
+            String fathersName = binding.fathersNameEditTextId.getText().toString();
+            String mothersHusbandWifeName = binding.mothersHusbandWifeNameEditTextId.getText().toString();
+            String village = binding.villageEditTextId.getText().toString();
+            String postOffice = binding.postOfficeEditTextId.getText().toString();
+            String thana = binding.thansEditTextId.getText().toString();
+            String district = binding.districtEditTextId.getText().toString();
+            String date_of_joining_job = binding.jobJoningDayTvId.getText().toString();
+            String oldWorksPlace = binding.oldWorksPlace1EditTextId.getText().toString();
+            String oldWorksPlace2 = binding.oldWorksPlace2EditTextId.getText().toString();
+            String date_of_joining_current_workplace = binding.currentWorkplaceJoningDayTvId.getText().toString();
+            String currentWorksPlace = binding.currentWorksPlaceEditTextId.getText().toString();
+            String facebookId = binding.facebookIdEditTextId.getText().toString();
+            String imoid = binding.imoidEditTextId.getText().toString();
+            String whatsappId = binding.whatsappIdEditTextId.getText().toString();
+
+         if(name.isEmpty()){
+             binding.nameEditTextId.requestFocus();
+             binding.nameEditTextId.setError("Name required");
+             return;
+         }else {
+             //insert data to database here
+
+
+             resetAllField();
+             Toast.makeText(this, "Contact add successfully", Toast.LENGTH_SHORT).show();
+
+         }
+
         });
-        binding.resetButtonId.setOnClickListener(view ->{
-           resetAllField();
+        binding.resetButtonId.setOnClickListener(view -> {
+            resetAllField();
             Toast.makeText(this, "Form Reset", Toast.LENGTH_SHORT).show();
         });
         binding.birthDateButtonId.setOnClickListener(view -> {
@@ -37,7 +74,7 @@ public class ContactAddActivity extends AppCompatActivity {
                 public void onDateSelected(String selectedDate) {
                     binding.birthDayTvId.setText(selectedDate);
                 }
-            },ContactAddActivity.this);
+            }, ContactAddActivity.this);
         });
         binding.jobJoningDateButtonId.setOnClickListener(view -> {
             AppUtils.setDate(new AppUtils.DatePickerCallback() {
@@ -45,7 +82,7 @@ public class ContactAddActivity extends AppCompatActivity {
                 public void onDateSelected(String selectedDate) {
                     binding.jobJoningDayTvId.setText(selectedDate);
                 }
-            },ContactAddActivity.this);
+            }, ContactAddActivity.this);
         });
         binding.currentWorkplaceJoningDateButtonId.setOnClickListener(view -> {
             AppUtils.setDate(new AppUtils.DatePickerCallback() {
@@ -53,7 +90,7 @@ public class ContactAddActivity extends AppCompatActivity {
                 public void onDateSelected(String selectedDate) {
                     binding.currentWorkplaceJoningDayTvId.setText(selectedDate);
                 }
-            },ContactAddActivity.this);
+            }, ContactAddActivity.this);
         });
     }
 
@@ -71,7 +108,7 @@ public class ContactAddActivity extends AppCompatActivity {
         binding.mobileNumber3EditTextId.setText("");
         binding.fathersNameEditTextId.setText("");
         binding.mothersHusbandWifeNameEditTextId.setText("");
-        binding.vaiilageEditTextId.setText("");
+        binding.villageEditTextId.setText("");
         binding.postOfficeEditTextId.setText("");
         binding.thansEditTextId.setText("");
         binding.districtEditTextId.setText("");
@@ -79,6 +116,7 @@ public class ContactAddActivity extends AppCompatActivity {
         binding.oldWorksPlace1EditTextId.setText("");
         binding.oldWorksPlace2EditTextId.setText("");
         binding.currentWorkplaceJoningDayTvId.setText("");
+        binding.currentWorksPlaceEditTextId.setText("");
         binding.facebookIdEditTextId.setText("");
         binding.imoidEditTextId.setText("");
         binding.whatsappIdEditTextId.setText("");
--- app/src/main/java/com/example/amirbp/Model/Contact.java
+++ app/src/main/java/com/example/amirbp/Model/Contact.java
@@ -77,8 +77,8 @@ public class Contact {
     public String thana;
 
     @Nullable
-    @ColumnInfo(name = "dristic")
-    public String dristic;
+    @ColumnInfo(name = "dristict")
+    public String dristict;
 
 
 
@@ -103,19 +103,277 @@ public class Contact {
     public String current_workplace;
 
     @Nullable
-    @ColumnInfo(name = "facebook_link")
-    public String facebook_link;
+    @ColumnInfo(name = "facebook_id")
+    public String facebook_id;
 
     @Nullable
-    @ColumnInfo(name = "whatsapp")
-    public String whatsapp;
+    @ColumnInfo(name = "whatsapp_id")
+    public String whatsapp_id;
 
     @Nullable
-    @ColumnInfo(name = "imo")
-    public String imo;
+    @ColumnInfo(name = "imo_id")
+    public String imo_id;
 
     @ColumnInfo(name = "date", defaultValue = "CURRENT_TIMESTAMP")
     public String date;
 
+    public Contact(@Nullable String case_number, String name, @Nullable String identity_type, @Nullable String rank, @Nullable String bp_number, @Nullable String nid, @Nullable String date_of_birth, @Nullable String bank_account_number, @Nullable String mobile_number, @Nullable String mobile_number_2, @Nullable String mobile_number_3, @Nullable String fathers_name, @Nullable String mother_husband_wife_name, @Nullable String village, @Nullable String post_office, @Nullable String thana, @Nullable String dristict, @Nullable String date_of_joining_job, @Nullable String old_workplace, @Nullable String old_workplace_2, @Nullable String date_of_joining_current_workplace, @Nullable String current_workplace, @Nullable String facebook_id, @Nullable String whatsapp_id, @Nullable String imo_id) {
+        this.case_number = case_number;
+        this.name = name;
+        this.identity_type = identity_type;
+        this.rank = rank;
+        this.bp_number = bp_number;
+        this.nid = nid;
+        this.date_of_birth = date_of_birth;
+        this.bank_account_number = bank_account_number;
+        this.mobile_number = mobile_number;
+        this.mobile_number_2 = mobile_number_2;
+        this.mobile_number_3 = mobile_number_3;
+        this.fathers_name = fathers_name;
+        this.mother_husband_wife_name = mother_husband_wife_name;
+        this.village = village;
+        this.post_office = post_office;
+        this.thana = thana;
+        this.dristict = dristict;
+        this.date_of_joining_job = date_of_joining_job;
+        this.old_workplace = old_workplace;
+        this.old_workplace_2 = old_workplace_2;
+        this.date_of_joining_current_workplace = date_of_joining_current_workplace;
+        this.current_workplace = current_workplace;
+        this.facebook_id = facebook_id;
+        this.whatsapp_id = whatsapp_id;
+        this.imo_id = imo_id;
+    }
 
+    @Nullable
+    public String getCase_number() {
+        return case_number;
+    }
+
+    public void setCase_number(@Nullable String case_number) {
+        this.case_number = case_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Nullable
+    public String getIdentity_type() {
+        return identity_type;
+    }
+
+    public void setIdentity_type(@Nullable String identity_type) {
+        this.identity_type = identity_type;
+    }
+
+    @Nullable
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(@Nullable String rank) {
+        this.rank = rank;
+    }
+
+    @Nullable
+    public String getBp_number() {
+        return bp_number;
+    }
+
+    public void setBp_number(@Nullable String bp_number) {
+        this.bp_number = bp_number;
+    }
+
+    @Nullable
+    public String getNid() {
+        return nid;
+    }
+
+    public void setNid(@Nullable String nid) {
+        this.nid = nid;
+    }
+
+    @Nullable
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(@Nullable String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    @Nullable
+    public String getBank_account_number() {
+        return bank_account_number;
+    }
+
+    public void setBank_account_number(@Nullable String bank_account_number) {
+        this.bank_account_number = bank_account_number;
+    }
+
+    @Nullable
+    public String getMobile_number() {
+        return mobile_number;
+    }
+
+    public void setMobile_number(@Nullable String mobile_number) {
+        this.mobile_number = mobile_number;
+    }
+
+    @Nullable
+    public String getMobile_number_2() {
+        return mobile_number_2;
+    }
+
+    public void setMobile_number_2(@Nullable String mobile_number_2) {
+        this.mobile_number_2 = mobile_number_2;
+    }
+
+    @Nullable
+    public String getMobile_number_3() {
+        return mobile_number_3;
+    }
+
+    public void setMobile_number_3(@Nullable String mobile_number_3) {
+        this.mobile_number_3 = mobile_number_3;
+    }
+
+    @Nullable
+    public String getFathers_name() {
+        return fathers_name;
+    }
+
+    public void setFathers_name(@Nullable String fathers_name) {
+        this.fathers_name = fathers_name;
+    }
+
+    @Nullable
+    public String getMother_husband_wife_name() {
+        return mother_husband_wife_name;
+    }
+
+    public void setMother_husband_wife_name(@Nullable String mother_husband_wife_name) {
+        this.mother_husband_wife_name = mother_husband_wife_name;
+    }
+
+    @Nullable
+    public String getVillage() {
+        return village;
+    }
+
+    public void setVillage(@Nullable String village) {
+        this.village = village;
+    }
+
+    @Nullable
+    public String getPost_office() {
+        return post_office;
+    }
+
+    public void setPost_office(@Nullable String post_office) {
+        this.post_office = post_office;
+    }
+
+    @Nullable
+    public String getThana() {
+        return thana;
+    }
+
+    public void setThana(@Nullable String thana) {
+        this.thana = thana;
+    }
+
+    @Nullable
+    public String getDristict() {
+        return dristict;
+    }
+
+    public void setDristict(@Nullable String dristict) {
+        this.dristict = dristict;
+    }
+
+    @Nullable
+    public String getDate_of_joining_job() {
+        return date_of_joining_job;
+    }
+
+    public void setDate_of_joining_job(@Nullable String date_of_joining_job) {
+        this.date_of_joining_job = date_of_joining_job;
+    }
+
+    @Nullable
+    public String getOld_workplace() {
+        return old_workplace;
+    }
+
+    public void setOld_workplace(@Nullable String old_workplace) {
+        this.old_workplace = old_workplace;
+    }
+
+    @Nullable
+    public String getOld_workplace_2() {
+        return old_workplace_2;
+    }
+
+    public void setOld_workplace_2(@Nullable String old_workplace_2) {
+        this.old_workplace_2 = old_workplace_2;
+    }
+
+    @Nullable
+    public String getDate_of_joining_current_workplace() {
+        return date_of_joining_current_workplace;
+    }
+
+    public void setDate_of_joining_current_workplace(@Nullable String date_of_joining_current_workplace) {
+        this.date_of_joining_current_workplace = date_of_joining_current_workplace;
+    }
+
+    @Nullable
+    public String getCurrent_workplace() {
+        return current_workplace;
+    }
+
+    public void setCurrent_workplace(@Nullable String current_workplace) {
+        this.current_workplace = current_workplace;
+    }
+
+    @Nullable
+    public String getFacebook_id() {
+        return facebook_id;
+    }
+
+    public void setFacebook_id(@Nullable String facebook_id) {
+        this.facebook_id = facebook_id;
+    }
+
+    @Nullable
+    public String getWhatsapp_id() {
+        return whatsapp_id;
+    }
+
+    public void setWhatsapp_id(@Nullable String whatsapp_id) {
+        this.whatsapp_id = whatsapp_id;
+    }
+
+    @Nullable
+    public String getImo_id() {
+        return imo_id;
+    }
+
+    public void setImo_id(@Nullable String imo_id) {
+        this.imo_id = imo_id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
